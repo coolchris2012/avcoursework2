d9fb30c4b8d92435f67e80b82dadb9ec6d7b51e3
EmmaG <62748853+tomgoodburn@users.noreply.github.com>
2025-11-27 14:47:22 +0000

    audio feature extraction changes

--- generateMFCCs.py
+++ generateMFCCs.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import featureExtraction
 import soundfile as sf
 
-# Generates MFCC values for all files in rawClips
+# Generates MFCC values for all files in Raw_Clips
 
-directory = "C:/Users/thoma/Documents/GitHub/AVCoursework1/rawClips"
+directory = "C:/Users/thoma/Documents/GitHub/AVCoursework2/Raw_Clips/audio"
 files = Path(directory).glob("*")
 for filename in files:
-  signal, sampleRate = sf.read("rawClips/" + filename.name, dtype='float32')
+  signal, sampleRate = sf.read("Raw_Clips/audio/" + filename.name, dtype='float32')
   name = filename.name.replace(".wav", "")
-  featureExtraction.main(signal, sampleRate, name, "MFCCs_07") # Manually iterate per run
+  featureExtraction.main(signal, sampleRate, name, "MFCCs_00") # Manually iterate per run
